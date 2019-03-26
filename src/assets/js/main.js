f351766c3a9fdb16266ccfd0e72877553aad22a3
geras <gerasymenkoph@gmail.com>
2019-03-26 21:49:10 +0200

    Update main.js

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,4 @@
-import 'bootstrap/dist/js/bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'jquery/dist/jquery'
-import "../scss/style.scss";
-import "./material";
+//import 'bootstrap/dist/js/bootstrap'
+//import 'bootstrap/dist/css/bootstrap.min.css'
+//import 'jquery/dist/jquery'
+//import "../scss/style.scss";
